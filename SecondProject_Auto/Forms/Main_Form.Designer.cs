c8d6d26a502b04fb72f06ac95a41b74d2f4aadc3
Камиль <gafur.kaml@gmail.com>
2025-05-03 11:44:34 +0300

    Test

--- SecondProject_Auto/Forms/Main_Form.Designer.cs
+++ SecondProject_Auto/Forms/Main_Form.Designer.cs
@@ -28,40 +28,44 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.title_lbl = new System.Windows.Forms.Label();
-            this.start_btn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.car_pbox = new System.Windows.Forms.PictureBox();
-            this.icon_pbox = new System.Windows.Forms.PictureBox();
-            ((System.ComponentModel.ISupportInitialize)(this.car_pbox)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.icon_pbox)).BeginInit();
+            this.start_btn = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.Login_btn = new System.Windows.Forms.Button();
+            this.register_btn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
-            // title_lbl
+            // label1
             // 
-            this.title_lbl.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.title_lbl.AutoSize = true;
-            this.title_lbl.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.title_lbl.Location = new System.Drawing.Point(47, 7);
-            this.title_lbl.Name = "title_lbl";
-            this.title_lbl.Size = new System.Drawing.Size(119, 24);
-            this.title_lbl.TabIndex = 2;
-            this.title_lbl.Text = "Car-Search";
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 16F, System.Drawing.FontStyle.Bold);
+            this.label1.Location = new System.Drawing.Point(47, 8);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(127, 26);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Car-Search";
             // 
-            // start_btn
+            // label3
             // 
-            this.start_btn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(106)))), ((int)(((byte)(232)))));
-            this.start_btn.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.start_btn.Font = new System.Drawing.Font("Lato Semibold", 12.25F, System.Drawing.FontStyle.Bold);
-            this.start_btn.ForeColor = System.Drawing.Color.White;
-            this.start_btn.Location = new System.Drawing.Point(64, 306);
-            this.start_btn.Name = "start_btn";
-            this.start_btn.Size = new System.Drawing.Size(122, 40);
-            this.start_btn.TabIndex = 5;
-            this.start_btn.Text = "Начать";
-            this.start_btn.UseVisualStyleBackColor = false;
-            this.start_btn.Click += new System.EventHandler(this.start_btn_Click);
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Lato Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(71, 187);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(450, 19);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Войдите в CarConnect для дополнительных возможностей";
             // 
             // label2
             // 
@@ -72,31 +76,78 @@
             this.label2.BackColor = System.Drawing.Color.Transparent;
             this.label2.Font = new System.Drawing.Font("Lato", 30.25F, System.Drawing.FontStyle.Bold);
             this.label2.ForeColor = System.Drawing.Color.White;
-            this.label2.Location = new System.Drawing.Point(55, 188);
+            this.label2.Location = new System.Drawing.Point(66, 138);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(758, 49);
+            this.label2.Size = new System.Drawing.Size(47, 49);
             this.label2.TabIndex = 3;
-            this.label2.Text = "Подбери себе идеальный автомобиль";
+            this.label2.Text = "ц";
             // 
-            // car_pbox
+            // start_btn
             // 
-            this.car_pbox.Image = global::SecondProject_Auto.Properties.Resources._123;
-            this.car_pbox.Location = new System.Drawing.Point(-3, 37);
-            this.car_pbox.Name = "car_pbox";
-            this.car_pbox.Size = new System.Drawing.Size(955, 528);
-            this.car_pbox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.car_pbox.TabIndex = 9;
-            this.car_pbox.TabStop = false;
+            this.start_btn.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.start_btn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(106)))), ((int)(((byte)(232)))));
+            this.start_btn.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.start_btn.Font = new System.Drawing.Font("Lato Semibold", 12.25F, System.Drawing.FontStyle.Bold);
+            this.start_btn.ForeColor = System.Drawing.Color.White;
+            this.start_btn.Location = new System.Drawing.Point(75, 267);
+            this.start_btn.Name = "start_btn";
+            this.start_btn.Size = new System.Drawing.Size(122, 40);
+            this.start_btn.TabIndex = 5;
+            this.start_btn.Text = "Начать";
+            this.start_btn.UseVisualStyleBackColor = false;
+            this.start_btn.Click += new System.EventHandler(this.start_btn_Click);
             // 
-            // icon_pbox
+            // pictureBox2
             // 
-            this.icon_pbox.Image = global::SecondProject_Auto.Properties.Resources.Icon;
-            this.icon_pbox.Location = new System.Drawing.Point(16, 6);
-            this.icon_pbox.Name = "icon_pbox";
-            this.icon_pbox.Size = new System.Drawing.Size(25, 25);
-            this.icon_pbox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
-            this.icon_pbox.TabIndex = 1;
-            this.icon_pbox.TabStop = false;
+            this.pictureBox2.Image = global::SecondProject_Auto.Properties.Resources.v8085f52;
+            this.pictureBox2.Location = new System.Drawing.Point(12, 48);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(924, 502);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 7;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Resize += new System.EventHandler(this.PictureBox2_Resize);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::SecondProject_Auto.Properties.Resources.Icon;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 8);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(25, 25);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Login_btn
+            // 
+            this.Login_btn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.Login_btn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(106)))), ((int)(((byte)(232)))));
+            this.Login_btn.Font = new System.Drawing.Font("Lato Semibold", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Login_btn.ForeColor = System.Drawing.Color.White;
+            this.Login_btn.Location = new System.Drawing.Point(639, 9);
+            this.Login_btn.Name = "Login_btn";
+            this.Login_btn.Size = new System.Drawing.Size(95, 33);
+            this.Login_btn.TabIndex = 7;
+            this.Login_btn.Text = "Войти";
+            this.Login_btn.UseVisualStyleBackColor = false;
+            this.Login_btn.Click += new System.EventHandler(this.Login_btn_Click);
+            // 
+            // register_btn
+            // 
+            this.register_btn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.register_btn.AutoSize = true;
+            this.register_btn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(106)))), ((int)(((byte)(232)))));
+            this.register_btn.Font = new System.Drawing.Font("Lato Semibold", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.register_btn.ForeColor = System.Drawing.Color.White;
+            this.register_btn.Location = new System.Drawing.Point(747, 9);
+            this.register_btn.Name = "register_btn";
+            this.register_btn.Size = new System.Drawing.Size(189, 33);
+            this.register_btn.TabIndex = 8;
+            this.register_btn.Text = "Зарегистрироваться";
+            this.register_btn.UseVisualStyleBackColor = false;
+            this.register_btn.Click += new System.EventHandler(this.register_btn_Click);
             // 
             // Main_Form
             // 
@@ -104,28 +155,32 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(948, 562);
+            this.Controls.Add(this.register_btn);
+            this.Controls.Add(this.Login_btn);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.start_btn);
-            this.Controls.Add(this.title_lbl);
-            this.Controls.Add(this.icon_pbox);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.car_pbox);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
-            this.MaximizeBox = false;
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.pictureBox2);
             this.Name = "Main_Form";
-            this.Text = "Стартовое окно";
-            ((System.ComponentModel.ISupportInitialize)(this.car_pbox)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.icon_pbox)).EndInit();
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-        private System.Windows.Forms.PictureBox icon_pbox;
-        private System.Windows.Forms.Label title_lbl;
-        private System.Windows.Forms.Button start_btn;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.PictureBox car_pbox;
+        private System.Windows.Forms.Button start_btn;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button Login_btn;
+        private System.Windows.Forms.Button register_btn;
+        private System.Windows.Forms.PictureBox pictureBox2;
     }
 }
 
